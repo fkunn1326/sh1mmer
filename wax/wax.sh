180f050e5805c7f30dec044f1af08b1f8caa6519
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2023-02-20 21:04:23 +0900

    Update wax.sh

--- wax/wax.sh
+++ wax/wax.sh
@@ -45,8 +45,8 @@ loop=$(losetup -f)
 losetup -P $loop $bin
 
 echo "Making arch partition"
-sleep 5 
-mkfs -t ext2 -L arch ${loop}p13 # ext2 so we can use skid protection features
+sleep 4
+mkfs.ext2 -L arch ${loop}p13 # ext2 so we can use skid protection features
 echo "Making ROOT mountable"
 sh make_dev_ssd_no_resign.sh --remove_rootfs_verification -i ${loop}
 echo "Creating Mountpoint"
